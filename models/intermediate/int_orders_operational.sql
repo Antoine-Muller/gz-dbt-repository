eb0828664bb296d8a0877d55ed6506bfaaac240b
antoine.muller974@gmail.com <antoine.muller974@gmail.com>
2024-10-30 08:22:09 +0000

    finance

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,11 +1,18 @@
 select
    s.orders_id,
    s.date_date,
-   ROUND ((margin + shipping_fee - logcost - ship_cost ),2) AS operational_margin,
-   quantity,
+   ROUND (s.margin + sh.shipping_fee - (sh.logcost + sh.ship_cost ),2) AS operational_margin,
+   s.quantity,
+   s.revenue,
+   s.purchase_cost,
+   s.margin,
+   sh.shipping_fee,
+   sh.logcost,
+   sh.ship_cost
 from {{ ref('int_orders_margin') }} as s
 left join {{ ref('stg_gz_ship') }} as sh
-on s.orders_id = sh.orders_id
+     USING(orders_id)
+ ORDER BY orders_id desc
 
 
-
+    
